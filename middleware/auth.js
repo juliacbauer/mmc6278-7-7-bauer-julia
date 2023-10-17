b5d70281d5c1e2f14eaacdfecf2540f35791d3b4
Julia Bauer <juliabauer@ufl.edu>
2023-10-17 16:56:17 -0400

    update middleware

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,7 +1,12 @@
 function checkAuth(req, res, next) {
 	// Check req.session.loggedIn to see if the user is logged in
+	if (req.session && req.session.isloggedIn) {
 	// If the user is logged in, call next()
+		return next()
 	// If the user is not logged in, redirect to /login
+	  } else {
+		res.redirect('/login');
+	  }
 }
 
 module.exports = checkAuth
